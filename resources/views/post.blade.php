ceeb8c66fde5d498b65011dc68c6ee6fb1163ec2
Yan Leandro <yanleandro658@gmail.com>
2022-02-23 09:33:31 +0100

    BLOG -Laravel

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="/css/post.css">
+    <article>
+        <h1>{{ $post->title }}</h1>
+
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+    <a href="/">Ir atras</a>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Post;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+#Nueva ruta: post.blade.php
+Route::get('/', function () {    
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post',[
+    
+        'post' => $post
+
+     
+
+    ]);
+});
+
+
+
